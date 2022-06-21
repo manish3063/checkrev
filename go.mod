53be7671f2ed2f8714afb7f6f87cd8b911a0590b
manish3063 <manishsingh111095@gmail.com>
2022-06-21 14:59:58 +0530

    revnum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module revnum
+
+go 1.18
---
+++ revnum.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := 456
+	output := reverse(input)
+	fmt.Println(output)
+
+}
+func reverse(input int) int {
+	var result int
+	isNegative := false
+
+	if input < 0 {
+		isNegative = true
+
+		input = input * -1
+	}
+	inputString := fmt.Sprint(input)
+	outputString := ""
+
+	inputStringArray := strings.Split(inputString, "")
+
+	for i := len(inputStringArray) - 1; i >= 0; i-- {
+		outputString = outputString + inputStringArray[i]
+
+	}
+	if isNegative {
+		outputString = "-" + outputString
+
+	}
+	result, _ = strconv.Atoi(outputString)
+	return result
+}
